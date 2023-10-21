861df1db053a09fe01313cd5a425f7ed5b45ad3a
vjencina <vjencina87@gmail.com>
2023-10-21 12:41:15 -0300

    Creación de sonido motor, Estados
    Se agrega sonido al motor de la nave, y se crea los Estados SPAWN,VIVO,INVENCIBLE,MUERTE, ajustando el script a lo deseado y conectando señales. Se anima el sprite ajustando la escala del mismo.

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,20 +1,30 @@
 class_name Player
 extends RigidBody2D
 
+##ENUMS
+enum ESTADO{SPAWN, VIVO, INVENCIBLE, MUERTO}
 
 ##Atributos Export
 export var potencia_motor :int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima: int = 250
 
 ##Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int =ESTADO.SPAWN
 
 ##Atributos onready
 onready var canion : Canion = $Canion
 onready var laser : RayoLaser = $LaserBeam2D
-
+onready var estela: Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor =$MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 ##Metodos
+#func _ready() -> void:
+	##controlador_estados(estado_actual)
+	##controlador_estados(ESTADO.VIVO)
+
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
@@ -29,8 +39,41 @@ func _unhandled_input(event: InputEvent) -> void:
 		laser.set_is_casting(true)
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
+#Control Estela y sonido motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(50)
+		motor_sfx.sonido_on()
+	
 ## Metodos Custom
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disable", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disable", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disable", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	estado_actual = nuevo_estado
+ 
+func esta_input_activo()-> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
+
 func player_input() -> void:
+	if not esta_input_activo():
+		return
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
@@ -51,3 +94,8 @@ func player_input() -> void:
 	
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
+
+## señales internas
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -12,12 +12,16 @@ onready var timer_enfriamiento:Timer = $TimerEnfriamiento
 onready var disparo_sfx:AudioStreamPlayer2D = $DisparoSFX
 onready var esta_enfriado:bool = true
 onready var esta_disparando:bool = false setget set_esta_disparando
+onready var puede_disparar: bool = false setget set_puede_disparar
 
 ## Atributos
 var puntos_disparo:Array = []
 ## Setter y Getters
 func set_esta_disparando(disparando:bool) -> void :
 	esta_disparando = disparando
+
+func set_puede_disparar(duenio_puede: bool)-> void:
+	puede_disparar = duenio_puede
 
 ## Metodos
 func _ready() -> void:
@@ -39,7 +43,7 @@ func disparar() -> void:
 	
 	for punto_disparo in puntos_disparo:
 		disparo_sfx.play()
-		var new_proyectil:ProyectilPlayer = proyectil.instance()
+		var new_proyectil:Proyectil = proyectil.instance()
 		new_proyectil.crear(
 			punto_disparo.global_position,
 			get_owner().rotation,
